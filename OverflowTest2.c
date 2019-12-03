2d5415595f563ea3c67ab1a3601b584d8d899832
Seongmin Park <psmin1206@gmail.com>
2019-12-03 17:51:56 +0900

    Add files via upload

---
+++ OverflowTest2.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	/* 분명 자료형 char는 -128 ~ 127의 값을 가지지만, 이 범위를 벗어난 수를 넣어줄 수도 있다.
+	   단, overflow가 일어난다는 점을 알아두어야 한다.*/
+	/* 자료형 char로 하면 %d나 %u로 4바이트 중에 3바이트를 1로 채운다.
+	   하지만 자료형 short로 하면 2바이트만을 사용한다. -> 왜 그럴까? */
+	char of = 129; //0b10000001 = -127
+	char c = -1; //0b11111111
+
+	printf("%d\n", of);     // -127
+	printf("%u\n", of);     // 4294967169
+	printf("%hhd\n", of);   // -127
+	printf("%hhu\n", of);   // 129
+	
+	char buffer[33];
+	_itoa(of, buffer, 2);
+
+	printf("decimal : %d \n", of);
+	printf("binary : %s \n", buffer);
+
+	return 0;
+}
+
